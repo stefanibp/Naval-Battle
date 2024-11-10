04e93179f3391c30e4252ba75bcd1059ef2ae264
stefania <stefania.bolanos@correounivalle.edu.co>
2024-11-09 22:42:36 -0500

    ship design update

--- src/main/java/com/example/navalbattle/view/Frigate.java
+++ src/main/java/com/example/navalbattle/view/Frigate.java
@@ -17,33 +17,29 @@ public class Frigate extends Application {
     public void start(Stage stage) {
 
         Pane root = new Pane();
-        Polygon frigate = new Polygon();
-        frigate.getPoints().addAll(
-                50.0, 50.0,   // Punto inferior izquierdo
-                75.0, 50.0,   // Punto inferior derecho
-                85.0, 37.5,   // Punto superior derecho
-                75.0, 25.0,   // Punto superior
-                50.0, 25.0    // Punto superior izquierdo
+        Polygon polygon = new Polygon();
+        polygon.getPoints().addAll(
+                50.0, 50.0,
+                75.0, 50.0,
+                85.0, 37.5,
+                75.0, 25.0,
+                50.0, 25.0
         );
 
-        frigate.setFill(Color.GREY);   // Color de relleno
-        frigate.setStroke(Color.BLACK); // Color del borde
-        frigate.setStrokeWidth(1.0);    // Grosor del borde
+        polygon.setFill(Color.DARKGRAY);
+        polygon.setStroke(Color.DARKGRAY);
+        root.getChildren().addAll(polygon);
 
-        Line line1 = Shape.lineStyle(50, 50, 75, 50, Color.BLACK, 2.0);   //  inferior
-        Line line2 = Shape.lineStyle(50, 50, 50, 25, Color.BLACK, 2.0);   //  izquierda
-        Line line3 = Shape.lineStyle(50, 25, 75, 25, Color.BLACK, 2.0);   //  superior
-        Line line4 = Shape.lineStyle(75, 25, 85, 37.5, Color.BLACK, 2.0); //  diagonal superior
-        Line line5 = Shape.lineStyle(85, 37.5, 75, 50, Color.BLACK, 2.0); //  diagonal inferior
-        Line line6 =  Shape.lineStyle(60, 42, 74, 42, Color.WHITE, 4.0);   // Línea inferior
-        Line line7 =  Shape.lineStyle(54, 45, 54, 30, Color.BLUE, 2.0);   //  izquierda
-        Line line8 =  Shape.lineStyle(60, 33, 74, 33, Color.WHITE, 4.0);   //  superior
-        Line line9 =  Shape.lineStyle(49, 41, 48, 41, Color.BLACK, 4.0);
-        Line line10 =  Shape.lineStyle(49, 34, 48, 34, Color.BLACK, 4.0);
+        Line line1 = Shape.lineStyle(50, 50, 50, 25, Color.DARKGRAY, 2.0);
+        Line line2 =  Shape.lineStyle(60, 42, 74, 42, Color.GREY, 4.0);
+        Line line3 =  Shape.lineStyle(54, 45, 54, 30, Color.GREY, 2.0);
+        Line line4 =  Shape.lineStyle(60, 33, 74, 33, Color.GREY, 4.0);
+        Line line5 =  Shape.lineStyle(49, 41, 48, 41, Color.DARKGRAY, 4.0);
+        Line line6 =  Shape.lineStyle(49, 34, 48, 34, Color.DARKGRAY, 4.0);
 
-        root.getChildren().addAll(line1, line2, line3, line4, line5, frigate, line6, line7, line8, line9, line10);
+        root.getChildren().addAll(line1, line2, line3, line4, line5, line6);
 
-        Scene scene = new Scene(root, 150, 100);
+        Scene scene = new Scene(root, 200, 200);
         stage.setScene(scene);
         stage.setTitle("Fragata");
         stage.show();
--- src/main/java/com/example/navalbattle/view/Submarine.java
+++ src/main/java/com/example/navalbattle/view/Submarine.java
@@ -1,4 +1,56 @@
 package com.example.navalbattle.view;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
 
-public class Submarine {
+public class Submarine extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Pane root = new Pane();
+
+        Rectangle body = Shape.square(50, 100, 90, 20, Color.DARKGRAY);
+        body.setArcWidth(40);
+        body.setArcHeight(40);
+        root.getChildren().add(body);
+
+        Polygon triangle1 = new Polygon();
+        triangle1.getPoints().addAll(
+                55.0, 105.0,
+                40.0, 110.0,
+                55.0, 115.0);
+
+        triangle1.setFill(Color.DARKGRAY);
+        root.getChildren().addAll(triangle1);
+
+
+        Ellipse propeller1 = new Ellipse(43, 110, 3, 8);
+        propeller1.setFill(Color.DARKGRAY);
+        root.getChildren().addAll(propeller1);
+
+        Ellipse hatch1 = Shape.ellipseStyle(60, 110, 3, 3, Color.WHITE);
+        Ellipse hatch2 = Shape.ellipseStyle(80, 110, 3, 3, Color.WHITE);
+        Ellipse hatch3 = Shape.ellipseStyle(115, 110, 3, 3, Color.WHITE);
+        root.getChildren().addAll(hatch1, hatch2, hatch3);
+
+
+        Line fins1 = Shape.lineStyle(115, 98, 115, 100, Color.DARKGRAY, 4.0);
+        Line fins2 = Shape.lineStyle(115, 122, 115, 120, Color.DARKGRAY, 4.0);
+        root.getChildren().addAll(fins1, fins2);
+
+        Scene scene = new Scene(root, 250, 200);
+        stage.setTitle("Submarine");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
